e616d68761632a3205dbfc50290ecde4b4fb1c52
sdsd34343sdfd33434343444445454dfdfdfsdf <neelam.chavan@smartbear.com>
2026-01-20 11:45:13 +0530

    Added 2 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://integration.stoplight-dev.com/api/v1/projects/cHJqOjcyODU0NA/spectral.js?branch_slug=main&token=e3f0bf7c-9d20-4266-a5f6-cc4fb130c3a0";
+export default { extends: ruleset };
